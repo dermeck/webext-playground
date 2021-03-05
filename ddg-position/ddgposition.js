e840fb654b0d94fd89861ff4021b41c322838db1
Martin <dermeck@posteo.de>
2021-03-05 11:41:11 +0100

    get search term from google search and fetch results from ddg

--- ddg-position/ddgposition.js
+++ ddg-position/ddgposition.js
@@ -1,1 +1,39 @@
-console.log("moep");
+// TODO XMLHttpRequest vs Fetch API
+
+const querySting = window.location.search;
+
+const urlParams = new URLSearchParams(querySting);
+
+if (urlParams.has("q")) {
+  const searchQuery = urlParams.get("q");
+
+  // search duckduckgo
+  let xhr = new XMLHttpRequest();
+  xhr.onload = function () {
+    let results = Array.from(
+      this.responseXML.getElementsByClassName("result")
+    ).map((result) => result.getElementsByClassName("result__a")[0].href);
+
+    // remove ads
+    results = results.filter(
+      (result) => !result.startsWith("https://duckduckgo.com/y.js")
+    );
+
+    applyResults(results);
+  };
+
+  xhr.onerror = (a, b) => {
+    console.log("an error occurred_", a, b);
+  };
+
+  console.log(searchQuery);
+
+  xhr.open("GET", "https://duckduckgo.com/html/?q=" + searchQuery, true);
+  xhr.responseType = "document";
+  xhr.send();
+}
+
+function applyResults(result) {
+  console.log("called applyResults");
+  console.log(result);
+}
