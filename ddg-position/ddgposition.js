c75519f66e562a1adb34440bf2037e386e09e2ec
Martin <dermeck@posteo.de>
2021-03-05 13:05:56 +0100

    add orange bo if google result also appears in ddg results

--- ddg-position/ddgposition.js
+++ ddg-position/ddgposition.js
@@ -1,5 +1,3 @@
-// TODO XMLHttpRequest vs Fetch API
-
 const querySting = window.location.search;
 
 const urlParams = new URLSearchParams(querySting);
@@ -9,31 +7,50 @@ if (urlParams.has("q")) {
 
   // search duckduckgo
   let xhr = new XMLHttpRequest();
-  xhr.onload = function () {
-    let results = Array.from(
-      this.responseXML.getElementsByClassName("result")
-    ).map((result) => result.getElementsByClassName("result__a")[0].href);
+  xhr.onload = (ev) => {
+    console.log(ev.target);
 
-    // remove ads
-    results = results.filter(
-      (result) => !result.startsWith("https://duckduckgo.com/y.js")
-    );
+    // get links from results and remove ads
+    let ddgResults = Array.from(
+      ev.target.responseXML.getElementsByClassName("result")
+    )
+      .map((result) => result.getElementsByClassName("result__a")[0].href)
+      .filter((result) => !result.startsWith("https://duckduckgo.com/y.js"));
 
-    applyResults(results);
+    applyResults(ddgResults);
   };
 
-  xhr.onerror = (a, b) => {
-    console.log("an error occurred_", a, b);
+  xhr.onerror = (ev) => {
+    console.log("an error occurred", ev);
   };
-
-  console.log(searchQuery);
 
   xhr.open("GET", "https://duckduckgo.com/html/?q=" + searchQuery, true);
   xhr.responseType = "document";
   xhr.send();
 }
 
-function applyResults(result) {
-  console.log("called applyResults");
-  console.log(result);
+function applyResults(ddgResults) {
+  console.log("ddgresults", ddgResults);
+
+  // compare with google results
+  let googleResults = Array.from(document.getElementsByClassName("g")).map(
+    (result) => result.getElementsByTagName("a")[0]
+  );
+
+  googleResults.forEach((googleResult) => {
+    // Check if URL appears in DDG results, if so, get position and create the div for the little orange box.
+    const ddgPosition = ddgResults.findIndex(
+      (element) => element === googleResult.href
+    );
+    console.log("position in ddgresults", ddgPosition, googleResult.href);
+
+    if (ddgPosition >= 0) {
+      googleResult.insertAdjacentHTML(
+        "beforeend",
+        '<div style="background-color: #de5833; position: absolute; top:0; right:0;"><p style="font-size: 15px; color: white; margin: 0; padding: 2px 9px 2px 9px;">' +
+          (ddgPosition + 1) +
+          "</p></div>"
+      );
+    }
+  });
 }
